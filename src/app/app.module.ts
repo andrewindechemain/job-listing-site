4ae141b9d31e6daf490dc2d93d0b247ed46074e4
AndrewIndeche <indecheandrew@outlook.com>
2023-11-27 11:00:18 +0300

    add responsive design to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponent,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
